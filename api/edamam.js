8a34c09e07662385f91193728de01bd197695d84
Laksh Jadhwani <lakshjadhwani.lj@gmail.com>
2024-03-31 16:08:49 -0700

    Completed Pagination

--- api/edamam.js
+++ api/edamam.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const API_ID = process.env.EDAMAM_API_ID;
 const API_KEY = process.env.EDAMAM_API_KEY;
 
-const getRecipes = async (query) => {
+const getRecipes = async (query, page, limit = 6) => {
 	try {
 		const response = await axios.get(`https://api.edamam.com/api/recipes/v2`, {
 			params: {
@@ -13,8 +13,23 @@ const getRecipes = async (query) => {
 				app_key: API_KEY,
 			},
 		});
+		const startIndex = (page - 1) * limit;
+		const endIndex = page * limit;
+		const results = response.data.hits.slice(startIndex, endIndex);
+		if (endIndex < response.data.hits.length) {
+			results.next = {
+				page: page + 1,
+				limit: limit,
+			};
+		}
+		if (startIndex > 0) {
+			results.prev = {
+				page: page - 1,
+				limit: limit,
+			};
+		}
 		console.log('Data fetched from Edamam api.');
-		return response.data.hits;
+		return results;
 	} catch (error) {
 		console.error(`Error fetching data from Edamam api: ${error}`);
 	}
--- routes/search.js
+++ routes/search.js
@@ -7,9 +7,16 @@ const { getRecipes } = require('../api/edamam');
 // @route   GET /search/:query
 router.get('/', async (req, res) => {
 	try {
-		query = req.query.query;
-		const recipes = await getRecipes(query);
-		res.render('results', { recipes, title: `Search: ${query}` });
+		const query = req.query.query;
+		const page = parseInt(req.query.page, 6) || 1;
+		const limit = parseInt(req.query.limit, 6) || 6;
+		const recipes = await getRecipes(query, page, limit);
+		res.render('results', {
+			recipes,
+			title: `Search: ${query}`,
+			query,
+			page,
+		});
 	} catch (error) {
 		console.error(`Error fetching data from Edamam api: ${error}`);
 		res.status(500).send('Error fetching recipes.');
